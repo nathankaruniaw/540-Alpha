6e76a8f951fbd7a4f4aceba2ff65cb5d19b76412
Yohanesbene <yohanes.benedictus19@gmail.com>
2023-04-15 19:20:58 +0700

    ui revision again

--- resources/views/portal/layouts/footer.blade.php
+++ resources/views/portal/layouts/footer.blade.php
@@ -2,27 +2,37 @@
 <footer class="pt-md-8 pb-md-5 py-5">
     <div class="container-fluid-custom">
         <div class="row">
-            <div class="col-md-5">
-                <img width="70%" src="assets/img/portal/White-Logo-Full.png" alt="" />
+            <div class="col-md-8">
+                <div class="footer-image-brand">
+                    <img src="assets/img/portal/White-Logo-Full.png" alt="" />
+                </div>
             </div>
-            <div class="col-md-7 mt-md-10 mb-md-5 my-5">
+            <div class="col-md-4 mt-md-10 mb-md-5 my-5">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-12">
+                        <div>
+                            <h4 class="heading-l text-white">Contact Us</h4>
+                            <p class="text-white mt-md-3 mt-3 mb-0">
+                                <a class="text-white" href="mailto:info@fivefortyalpha.com">info@fivefortyalpha.com</a>
+                            </p>
+                        </div>
+                    </div>
+                    <!-- <div class="col-md-6">
                         <h4 class="heading-l text-white">Indonesia</h4>
                         <p class="text-white mt-5 mb-0">(Office Address)</p>
                         <p class="text-m text-white">
                             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, diam
                             nonummy nibh euismod
                         </p>
-                    </div>
-                    <div class="col-md-6">
+                    </div> -->
+                    <!-- <div class="col-md-6">
                         <h4 class="heading-l text-white">Singapore</h4>
                         <p class="text-white mt-5 mb-0">(Office Address)</p>
                         <p class="text-m text-white">
                             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, diam
                             nonummy nibh euismod
                         </p>
-                    </div>
+                    </div> -->
                 </div>
             </div>
             <div class="col-xs-12 col-md-6 mb-4">
