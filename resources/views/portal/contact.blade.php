276192e85ac91c169db01a7f1eb2afa777a87820
liemdaniel203 <liemdaniel203@gmail.com>
2023-05-01 16:47:17 +0700

    [FIX] Contact

--- resources/views/portal/contact.blade.php
+++ resources/views/portal/contact.blade.php
@@ -31,12 +31,19 @@
         <div class="col-xs-12 col-md-11">
           <div class="form-wrapper">
             <form action="#">
+              <input type="text" id="orng-grouped" name="orng-grouped" value="Startup Owner" hidden>
               <fieldset>
+                <div class="row px-3">
+                  <button type="button" class="orng-radio-2 orng-radio-2-focus col-6" data-text="Startup Owner">Startup Owner</button>
+                  <button type="button" class="orng-radio-2  orng-radio-2-unfocus col-6" data-text="Investor">Investor</button>
+                </div>
+              </fieldset>
+              <!-- <fieldset>
                 <span class="orng-radio-group">
                   <input class="orng-radio" type="radio" name="orng-grouped" data-text="Startup Owner" checked />
                   <input class="orng-radio" type="radio" name="orng-grouped" data-text="Investor" />
                 </span>
-              </fieldset>
+              </fieldset> -->
               <fieldset>
                 <input type="text" id="fname" name="fname" placeholder="Full name" required />
               </fieldset>
@@ -46,8 +53,17 @@
               <fieldset>
                 <input type="text" id="phone" name="phone" placeholder="Phone" required />
               </fieldset>
-              <fieldset>
-                <input type="text" id="company-name" name="company-name" placeholder="Company name" />
+              <fieldset id="company_name">
+                <input type="text" name="company_name" placeholder="Company Name"/>
+              </fieldset>
+              <fieldset id="startup_name">
+                <input type="text" name="startup_name" placeholder="Startup Name" />
+              </fieldset>
+              <fieldset id="startup_location">
+                <input type="text" name="startup_location" placeholder="Startup Location (country)" />
+              </fieldset>
+              <fieldset id="startup_category">
+                <input type="text" name="startup_category" placeholder="Startup Category/Industry" />
               </fieldset>
               <div class="button-wrapper">
                 <button type="submit" class="secondary-button">Submit</button>
@@ -59,6 +75,34 @@
     </div>
   </section>
   @include('portal.layouts.footer')
+  <script>
+    $(document).ready(function() {
+      $('#company_name').hide();
+      
+      $('.orng-radio-2').click(function() {
+
+        $('.orng-radio-2-focus').addClass('orng-radio-2-unfocus')
+        $('.orng-radio-2-focus').removeClass('orng-radio-2-focus')
+        $('#orng-grouped').val($(this).data('text'))
+
+        $(this).removeClass('orng-radio-2-unfocus')
+        $(this).addClass('orng-radio-2-focus')
+
+        if($(this).data('text') != 'Startup Owner'){
+          $('#startup_name').hide();
+          $('#startup_location').hide();
+          $('#startup_category').hide();
+          $('#company_name').show();
+        }
+        else{
+          $('#startup_name').show();
+          $('#startup_location').show();
+          $('#startup_category').show();
+          $('#company_name').hide();
+        }
+      })
+    })
+  </script>
 
 </body>
 
